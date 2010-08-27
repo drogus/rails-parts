8fe69d5f094a1432f6adf206f7d9f200a9fbe0c5
Nicolas Morel <incubatio@gmail.com>
2010-08-27 18:36:00 +0800

    remove controller_name var which not exists anymore thanks to alx

--- lib/parts/default_layout.rb
+++ lib/parts/default_layout.rb
@@ -2,8 +2,7 @@ module Parts
   module DefaultLayout
     def _default_layout(require_layout = false)
       layout_name = super
-
-      layout_name || _layout || controller_name
+      layout_name || _layout
     end
   end
 end
