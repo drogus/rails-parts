1ca3fffd28e6c8256eb3e500d38b266b6639fa75
Dylan Stamat <dstamat@elctech.com>
2010-09-14 18:13:12 -0700

    Pushing Parts::Helpers into ActionView::Base in railtie.rb.  This allows a Parts template to reference a Part.

--- lib/parts/railtie.rb
+++ lib/parts/railtie.rb
@@ -5,6 +5,7 @@ module Parts
   class Railtie < Rails::Railtie
     initializer "parts.include_helpers" do |app|
       ActiveSupport.on_load(:action_controller) do
+        ActionView::Base.send(:include, Parts::Helpers)
         ActionController::Base.send(:include, Parts::Helpers)
         ActionController::Base.helper_method(:part)
       end
